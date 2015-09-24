0d07fbd2d8bf14153dc9c016fece79acd847183b
gudun-ku <gudun.ku@gmail.com>
2015-09-24 16:35:10 +0300

    colors and line styles changed

--- Plot2.R
+++ Plot2.R
@@ -28,21 +28,20 @@ plot(names(NEI_Balt_total),
      main = "Total PM2.5 Emissions in Baltimore County",
      xlab = "",
      ylab = "PM2.5 Emissions",
-     pch = 18, col = "red",
-     lty = 5,
+     pch = 18, col = "steelblue",
      lwd = 2,
      sub = "Total emission level in Baltimore county have decreased in range from 1999 to 2008")
 
 ## use lm to make trend line on plot, to show it. 
 lmodel <- lm(NEI_Balt_total ~ as.integer(names(NEI_Balt_total)))
-abline(lmodel,col = "darkgreen", lwd = 2)
+abline(lmodel,col = "darkred", lwd = 2, lty = 8)
 
 ## add legend to plot
 legend(2005,3325, 
        c("Emission","Trend"), 
-       lty=c(5,1),
+       lty=c(1,5),
        lwd=c(2,2),
-       col=c("red","darkgreen")) 
+       col=c("steelblue","darkred")) 
 
 
 dev.copy(png,"Plot2.png", width = 800, height = 600)
